f75a1248ee4aa81ef1ab311ce1f3f5b5279cbf17
Servo <louis.servelle@gmail.com>
2022-09-22 22:06:50 +0200

    index.js

---
+++ Pierre/src/js/index.js
@@ -0,0 +1,17 @@
+function  burgermenu(){
+    let burgerMenu = document.getElementById("burger-menu");
+    burgerMenu.addEventListener("click", )
+}
+
+function users(){
+    let users = document.getElementById("users");
+    users.addEventListener("click", )
+}
+
+function cart(){
+    let cart = document.getElementById("cart");
+    cart.addEventListener("click", () => {
+        
+    })
+
+}
